75f23a6955157482880d985e29a795026588f44c
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2021-09-28 22:03:34 +0100

    Initial Commit

---
+++ rprename/views.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# rprename/views.py
+
+"""This module provides the RP Renamer main window."""
+
+from collections import deque
+from pathlib import Path
+
+from PyQt5.QtWidgets import QWidget, QFileDialog
+
+from .ui.window import Ui_Window
+
+FILTERS = ";;".join(
+    (
+        "PNG Files (*.png)",
+        "JPEG Files (*.jpeg)",
+        "JPG Files (*.jpg)",
+        "GIF Files (*.gif)",
+        "Text Files (*.txt)",
+        "Python Files (*.py)",
+    )
+)
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self._files = deque()
+        self._filesCount = len(self._files)
+        self.ui = Ui_Window()
+        self._setupUI()
+        self._connectSignalsSlots()
+
+    def _setupUI(self):
+        self.ui.setupUi(self)
+
+    def _connectSignalsSlots(self):
+        self.loadFilesButton.clicked.connect(self.loadFiles)
+
+    def loadFiles(self):
+        self.dstFileList.clear()
+        if self.dirEdit.text():
+            initDir = self.dirEdit.text()
+        else:
+            initDir = str(Path.home())
+        files, filter = QFileDialog.getOpenFileNames(
+            self, "Choose Files to Rename", initDir, filter=FILTERS
+        )
+        if len(files) > 0:
+            fileExtension = filter[filter.index("*"): -1]
+            self.extensionLabel.setText(fileExtension)
+            srcDirName = str(Path(files[0]).parent)
+            self.dirEdit.setText(srcDirName)
+            for file in files:
+                self._files.append(Path(file))
+                self.srcFileList.addItem(file)
+            self._filesCount = len(self._files)
